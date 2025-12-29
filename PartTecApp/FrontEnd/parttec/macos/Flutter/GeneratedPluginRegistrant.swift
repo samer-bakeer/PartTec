64d9a878986b1c633943f152c4df64e0a4a202e3
samer-bakeer <lord57881@gmail>
2025-12-29 11:00:29 +0100

    uguy

--- PartTecApp/FrontEnd/parttec/macos/Flutter/GeneratedPluginRegistrant.swift
+++ PartTecApp/FrontEnd/parttec/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import file_selector_macos
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
+import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
