380caaf86d05c2aeeb40e9e655b81857b07b77b6
samer-bakeer <lord57881@gmail>
2026-02-17 13:56:26 +0300

    m

--- PartTecApp/FrontEnd/parttec/macos/Flutter/GeneratedPluginRegistrant.swift
+++ PartTecApp/FrontEnd/parttec/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,28 +7,18 @@ import Foundation
 
 import file_picker
 import file_selector_macos
-<<<<<<< HEAD
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import shared_preferences_foundation
 import webview_flutter_wkwebview
-=======
-import flutter_local_notifications
-import shared_preferences_foundation
->>>>>>> ac73e6abfcccd9fcdd4deef18c37b6cb8c431764
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-<<<<<<< HEAD
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
-=======
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> ac73e6abfcccd9fcdd4deef18c37b6cb8c431764
 }
